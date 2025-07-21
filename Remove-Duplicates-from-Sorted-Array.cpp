9039bc99c618fdc6733b3ac88c7f8a096c3696e5
Abdelrahman Elsafty <151095160+abdelrahmanelsafty75@users.noreply.github.com>
2025-07-21 23:05:31 +0300

    [26] [Time Beats: 8.07%] [Memory Beats: 10.14%] - LeetPush

---
+++ Remove-Duplicates-from-Sorted-Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int>st;
+        for(int i=0 ; i<nums.size();i++)
+        st.insert(nums[i]);
+        nums.assign(st.begin(),st.end());
+        return nums.size();
+    }
+};

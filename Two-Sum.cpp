ded3c1292e9b81948890e0bcd2ee29a493556a21
Abdelrahman Elsafty <151095160+abdelrahmanelsafty75@users.noreply.github.com>
2025-07-22 00:21:48 +0300

    [1] [Time Beats: 100.00%] [Memory Beats: 36.66%] - LeetPush

--- Two-Sum.cpp
+++ Two-Sum.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-     int n = nums.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
-            }
+        unordered_map<int,int>mp;
+        for(int i=0; i<nums.size(); i++){
+            int result = target - nums[i];
+
+            if(mp.find(result) != mp.end())
+            return{i,mp[result]};
+
+            mp[nums[i]] = i;
         }
-        return {}; 
+        return{};
     }
 };

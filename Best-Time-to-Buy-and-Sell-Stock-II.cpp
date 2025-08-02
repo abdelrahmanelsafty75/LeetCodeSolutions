17f4212a6b92567be8d192a6ac0046ee715e1a97
Abdelrahman Elsafty <151095160+abdelrahmanelsafty75@users.noreply.github.com>
2025-08-02 21:26:24 +0300

    [122] [Time Beats: 100.00%] [Memory Beats: 98.14%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock-II.cpp
+++ Best-Time-to-Buy-and-Sell-Stock-II.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& v) {
-        int ans = 0 , cur = v[0];
+        int ans = 0;
         const int n = v.size();
         for(int i=1 ;i< n;i++){
-            if(v[i] < cur){
-                cur = v[i];
-            }
-                else{
-                ans+=(v[i]-cur);
-                cur=v[i];
-            }
+          if(v[i] > v[i-1])
+          ans+= v[i] - v[i-1];
         }
         return ans;
     }

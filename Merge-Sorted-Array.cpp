936e8f30e57207ef5420fc16328445dfe6f4047a
Abdelrahman Elsafty <151095160+abdelrahmanelsafty75@users.noreply.github.com>
2025-08-18 03:37:22 +0300

    [88] [Time Beats: 100.00%] [Memory Beats: 70.33%] - LeetPush

--- Merge-Sorted-Array.cpp
+++ Merge-Sorted-Array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i = m , j = 0;i < n+m ;i++ , j++)
-        nums1[i] = nums2[j];
+        for(int i = 0; i < n ;i++ )
+        nums1[i+m] = nums2[i];
     sort(nums1.begin(),nums1.end());
     }
 };

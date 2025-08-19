90aedb04653feb2db8bbc4a2ba7610d8e9a9bcf7
Abdelrahman Elsafty <151095160+abdelrahmanelsafty75@users.noreply.github.com>
2025-08-20 00:59:22 +0300

    [1768] [Time Beats: 100.00%] [Memory Beats: 63.33%] - LeetPush

--- Merge-Strings-Alternately.cpp
+++ Merge-Strings-Alternately.cpp
@@ -2,25 +2,16 @@ class Solution {
 public:
     string mergeAlternately(string s, string t) {
       string ans = "";
-      string ret = "";
-      int smx =0 , smn=0;
-      if(s.size() > t.size()){
-        smx = s.size();
-        smn = t.size();
-        ret = s;
+      const int sz1 = s.size();
+      const int sz2 = t.size();
+      int l = 0;
+      while(l < sz1 || l < sz2){
+         if( l < sz1)
+           ans += s[l];      
+        if(l <sz2 )
+            ans += t[l];   
+        l++;
       }
-      else{
-        smx = t.size();
-        smn = s.size();
-        ret = t;
-      }
-    for(int i =0 ; i< smn ;i ++){
-        ans += s[i];
-        ans += t[i];
-    }
-    for( int i = smn ; i<smx ;i ++)
-        ans +=ret[i];
-
-        return ans;
+      return ans;
     }
 };

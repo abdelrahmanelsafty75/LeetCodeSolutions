55097337b863843f703ba1b7f155e6181ee25fb4
Abdelrahman Elsafty <151095160+abdelrahmanelsafty75@users.noreply.github.com>
2025-09-12 23:53:57 +0300

    [594] [Time Beats: 32.92%] [Memory Beats: 52.02%] - LeetPush

--- Longest-Harmonious-Subsequence.cpp
+++ Longest-Harmonious-Subsequence.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        for (int i : nums)freq[i]++;
+        unordered_map<int,int>freq;
         int ans = 0;
-        for (auto& [n, c] : freq) {
-            if (freq.count(n + 1)) {
-                int currentLength = c + freq[n + 1];
-                ans = max(ans, currentLength);
+        for(int i : nums) freq[i]++;
+        for(auto& [n,c] : freq){
+            if(freq.count(n+1)>0){
+                int maxlen = c + freq[n+1];
+                ans = max(ans,maxlen);
             }
         }
-
         return ans;
     }
 };
